bf290d8ee57c4939f77be8137ffc0a06ba655dcb
HanifNahan <muhammad.hanif.mh639@gmail.com>
2024-02-08 03:54:46 +0800

    commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-h.png';
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import Logo from './Logo';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = 'anif'.split('')
+    const jobArray = "I'm a".split('')
+    const devArray = 'Software Developer'.split('')
+
+    useEffect(() => {
+        return () => setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 4000)
+      }, [])
+
+    return (
+        <>
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={20}
+                    />
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={devArray}
+                        idx={10}
+                    />
+                </h1>
+                <h2>I'm a self-taught programmer</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo />
+        </div>
+        <Loader type='pacman' />
+        </>
+    )
+}
+
+export default Home
