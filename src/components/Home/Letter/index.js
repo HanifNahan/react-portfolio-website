581b90941f5a5ad18ea8fdca56fc207ef3b5a580
hanif <hanif@glsb.my>
2024-02-08 17:14:25 +0800

    edit animantion

---
+++ src/components/Home/Letter/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Letter = () => {
+    return (
+        <div className='letter-container'>
+            <p>mmmmmmmmmmmmmmmmmmmmmm</p>
+        </div>
+    );
+}
+
+export default Letter;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import { Link } from 'react-router-dom'
-// import LogoTitle from '../../assets/images/logo-h.png';
 import AnimatedLetters from '../AnimatedLetters'
+import Letter from './Letter'
 import './style.scss'
-// import Logo from './Logo';
+import Logo from './Logo';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -50,7 +50,8 @@ const Home = () => {
                 <h2>I'm a self-taught programmer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-            {/* <Logo /> */}
+            <Letter />
+            <Logo />
         </div>
         <Loader type='pacman' />
         </>
